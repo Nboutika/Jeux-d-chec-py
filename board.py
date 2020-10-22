030d2d23f255f1d10db5100d924a7260fa596aa2
ARIES Lucas <lucas.aries@sfr.fr>
2020-10-22 23:21:42 +0200

    Création class BoardCoord

--- board.py
+++ board.py
@@ -12,7 +12,14 @@
 #On fait comment le plateau ? 
 #Quel code ? 
 
-BoardCoord=[
+
+
+
+class BoardCoord :
+
+        "Le plateau d'echec"
+
+    BoardCoord=[
         'a8','b8','c8','d8','e8','f8','g8','h8',
         'a7','b7','c7','d7','e7','f7','g7','h7',
         'a6','b6','c6','d6','e6','f6','g6','h6',
