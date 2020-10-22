6dd04dda2946278d0d9558c40b3396e82aedc379
nassim boutikar <boutikarnassim@gmail.com>
2020-10-23 01:43:54 +0200

    Ajout de règle

--- board.py
+++ board.py
@@ -9,8 +9,12 @@
 # 2   p p p p p p p p
 # 1   T C F r R F C T
 
-# On fait comment le plateau ?
-# Quel code ?
+"""
+Utilisation d'unicode pour les pièces (bien vérifier si l'encodage est l'UTF-8)
+Chercher une solution pour les cases vides qui est agréable au regard pour pas que l'on se perde dans l'échiquier
+(éviter le caractère unicode du point central par exemple qui visiblement n'est pas adapté)
+Choix des coordonnées : Tableau[ligne][colonne] /!\ Attention ici en python l'index commence à 0 et non pas à 1 ! /!\
+"""
 
 
 class BoardCoord:
