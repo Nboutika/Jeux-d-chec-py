56b5633d3f9768ecb5b9bef1085f8aba0fef0db7
Kallu-A <73198738+Kallu-A@users.noreply.github.com>
2020-10-23 09:36:46 +0200

    Rectification affichage

--- board.py
+++ board.py
@@ -23,7 +23,7 @@ class BoardCoord:
 
 """
     Représentation des coordonnées du plateau : 
-    0  1 2  3 4  5 6 7 
+    0 1 2 3 4 5 6 7 
 ---------------------
 0|  ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
 1|  ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎
