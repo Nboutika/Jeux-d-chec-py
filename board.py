1559c1e5acd9833cc883e5ee7e8cd66810b74525
ARIES Lucas <lucas.aries@sfr.fr>
2020-10-23 17:21:57 +0200

    Version Avancé save

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-
+from pieces import *
 
 """
 Utilisation d'unicode pour les pièces (bien vérifier si l'encodage est l'UTF-8)
@@ -23,7 +23,7 @@ class Board():
         ]
 
 """
-    Représentation des coordonnées du pLigneArriveteau : 
+    Représentation des coordonnées du plateau: 
     0 1 2 3 4 5 6 7 
 ---------------------
 0|  ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
@@ -39,7 +39,7 @@ class Board():
 b = Board()
 
 
-"""
+print("Pour jouez utiliser |ligne de la pièce| |colonne de la pièce| |ligne d'arrivé| |colonne d'arrivé|")
 commande = input("Jouez votre coup : ").split(" ")
 commandeTableau = []
 
@@ -47,27 +47,22 @@ for i in commande:
     if i != "":
         commandeTableau.append(int(i))
 
-print(commandeTableau)
+for i in range(len(commandeTableau)) :
+    if commandeTableau[i]<0 or commandeTableau[i]>7 :
+        print("Pas dans l'intervalle")
+    else :
+        print("Dans l'intervalle")
+
+ligne=commandeTableau[0]
+colonne=commandeTableau[1]
+ligneArrive=commandeTableau[2]
+colonneArrive=commandeTableau[3]
+
+print(ligne,colonne,ligneArrive,colonneArrive)
 
 """
 
-
-ligne=int(input("Veuillez saisir un entier entre [0,7] pour la ligne de départ : "))
-while ligne<0 or ligne>7 :
-    ligne=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour la ligne de départ : "))
-
-colonne=int(input("Veuillez saisir un entier entre [0,7] pour la colonne désirée : "))
-while colonne<0 or colonne>7 : 
-    colonne=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour la colonne désirée : "))
-
-
-ligneArrive=int(input("Veuillez saisir un entier entre [0,7] pour la nouvelle ligne désirée : "))
-while ligneArrive<0 or ligneArrive>7 :
-    ligneArrive=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour la nouvelle ligne désirée : "))
-
-colonneArrive=int(input("Veuillez saisir un entier entre [0,7] pour  la nouvelle colonne désirée : "))
-while colonneArrive<0 or colonneArrive>7 : 
-    colonneArrive=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour  la nouvelle colonne désirée : "))
+print(ligne,colonne,ligneArrive,colonneArrive)
 
 
 if ligne==ligneArrive and colonne==colonneArrive:
@@ -75,39 +70,88 @@ if ligne==ligneArrive and colonne==colonneArrive:
 else:
     if b.BoardCoord[ligne][colonne]== "♜":
         print("tour blanche")
+        if tour(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
+
 
     if b.BoardCoord[ligne][colonne]== "♞":
         print("cavalier blanc")
+        if cavalier(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♝":
         print("fou blanc")
+        if fou(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")        
 
     if b.BoardCoord[ligne][colonne]== "♛":
         print("reine blanc")
+        if dame(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♚":
         print("roi blanc")
+        if roi(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♟︎":
         print("pion blanc ")
+        if pionNoir(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) : #pas encore de fonction pion blanc
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♖":
         print("tour noir")
+        if tour(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♘":
         print("cavalier noir")
+        if cavalier(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♗":
         print("fou noir")
+        if fou(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♕":
         print("reine noir")
+        if dame(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♔":
         print("roi noir")
+        if roi(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
     if b.BoardCoord[ligne][colonne]== "♙":
         print("pion noir ")
+        if pionNoir(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+            print("mouvement possible")
+        else:
+            print("mouvement pas possible")
 
 
 
@@ -122,3 +166,4 @@ for l in range(8) :
 
 print("   a  b  c  d  e  f  g  h")
 
+"""
---
+++ pieces.py
@@ -0,0 +1,46 @@
+"""
+Initialisation des pièces
+La tour : Elle se déplace en ligne et colonne([+x][+0] OU [+0][+x])
+Le cavalier : Il se déplace en "L" donc il se déplace de 2 lignes et 1 colonne([1][0]) mais dans tous les sens 
+Donc +-[2]+-[1] ET aussi +-[1]+-[2]
+Le fou : il se déplace toujours en diagonale [+x][+x]
+La dame : Elle a tous les mouvements des autres pièces sauf le cavalier
+Le roi : comme la dame mais de 1 en 1
+Pion : les noirs se déplace en ligne [+x][0]
+Pion blanc : [-x][0]
+"""
+# l = ligne départ
+# c = colonne départ
+# la, colonneArrive = ligne/colonne arrivé
+"""
+boardCoord = []
+ligne = None
+colonne = None
+colonneArrive = None
+ligneArrive = None"""
+
+def vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return boardCoord[ligneArrive][colonneArrive] == "-"
+
+def pionNoir(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return (colonne == colonneArrive and ligneArrive == ligne + 1) and vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)
+
+
+def tour(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return ligne == ligneArrive or colonne == colonneArrive and vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)
+
+
+def cavalier(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return (ligne - 2 <= ligneArrive <= ligne + 2 and colonne - 1 <= colonneArrive <= colonne + 1) or (colonne - 2 <= colonneArrive <= colonne + 2 and ligne - 1 <= ligneArrive <= ligne + 1) and vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)
+
+
+def fou(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return colonneArrive == ligneArrive and vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)
+
+
+def dame(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return (fou(boardCoord,  ligne, colonne, ligneArrive, colonneArrive) or tour(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)) and vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)
+
+
+def roi(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
+    return ((ligne - 1 <= ligneArrive <= ligne + 1) and (colonne - 1 <= colonne <= colonne + 1)) and vide(boardCoord,  ligne, colonne, ligneArrive, colonneArrive)
