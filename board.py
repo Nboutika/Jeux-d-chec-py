4b9f550998113129295e9ca92371d51ae4d4d2cd
ARIES Lucas <lucas.aries@sfr.fr>
2020-10-23 14:12:38 +0200

    Affichage plateau en print

--- board.py
+++ board.py
@@ -8,18 +8,19 @@ Choix des coordonnées : Tableau[ligne][colonne] /!\ Attention ici en python l'i
 """
 
 
-class BoardCoord:
 
-    BoardCoord = [
-        ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
-        ['♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎'],
-        ["-", "-", "-", "-", "-", "-", "-", "-"],
-        ["-", "-", "-", "-", "-", "-", "-", "-"],
-        ["-", "-", "-", "-", "-", "-", "-", "-"],
-        ["-", "-", "-", "-", "-", "-", "-", "-"],
-        ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-        ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
-    ]
+class Board():
+    def __init__(self):
+        self.BoardCoord = [
+            ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+            ['♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎', '♟︎'],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+            ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
+        ]
 
 """
     Représentation des coordonnées du plateau : 
@@ -27,15 +28,18 @@ class BoardCoord:
 ---------------------
 0|  ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
 1|  ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎
-2|  - - - - - - - -
-3|  - - - - - - - -
-4|  - - - - - - - -
-5|  - - - - - - - -
+2|  -  - -  - - -  - -
+3|  -  - -  - - -  - -
+4|  -  - -  - - -  - -
+5|  -  - -  - - -  - -
 6|  ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
 7|  ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 """
 
-b = BoardCoord()
+b = Board()
+
+
+
 
 l=int(input("Veuillez saisir un entier entre [0,7] pour la ligne désirée : "))
 while l<0 or l>7 :
@@ -45,9 +49,24 @@ c=int(input("Veuillez saisir un entier entre [0,7] pour  la colonne désirée : 
 while c<0 or c>7 : 
     c=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour  la colonne désirée : "))
 
+
+la=int(input("Veuillez saisir un entier entre [0,7] pour la nouvelle ligne désirée : "))
+while la<0 or la>7 :
+    la=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour la nouvelle ligne désirée : "))
+
+ca=int(input("Veuillez saisir un entier entre [0,7] pour  la nouvelle colonne désirée : "))
+while ca<0 or ca>7 : 
+    ca=int(input("Nombre incorrect , veuillez ressaisir un entier entre [0,7] pour  la nouvelle colonne désirée : "))
+    
 print(b.BoardCoord[l][c])
+b.BoardCoord[la][ca]=b.BoardCoord[l][c]
+b.BoardCoord[l][c]="-"
 
 for ligne in range(8) :
+    print(ligne," ",end="")
     for colonne in range(8) :
-        print(b.BoardCoord[ligne][colonne]," ",end='')
+        print(b.BoardCoord[ligne][colonne]," ",end="")
     print("")
+
+print("   a  b  c  d  e  f  g  h")
+
