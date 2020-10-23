4cf21fe5b876196c96286ead756fdb015d4cde65
nassim boutikar <boutikarnassim@gmail.com>
2020-10-23 14:46:53 +0200

    Déplacement des pièces(textuel)

--- pieces.py
+++ pieces.py
@@ -1,3 +1,36 @@
 """
 Initialisation des pièces
+
+La tour : Elle se déplace en ligne et colonne([+x][+0] OU [+0][+x])
+Le cavalier : Il se déplace en "L" donc il se déplace de 2 lignes et 1 colonne([1][0]) mais dans tous les sens 
+Donc +-[2]+-[1] ET aussi +-[1]+-[2]
+Le fou : il se déplace toujours en diagonale [+x][+x]
+La dame : Elle a tous les mouvements des autres pièces sauf le cavalier
+Le roi : comme la dame mais de 1 en 1
+Pion : les noirs se déplace en ligne [+x][0]
+Pion blanc : [-x][0]
 """
+
+
+def Pion():
+    pass
+
+
+def Tour():
+    pass
+
+
+def Cavalier():
+    pass
+
+
+def Fou():
+    pass
+
+
+def Dame():
+    pass
+
+
+def Roi():
+    pass
