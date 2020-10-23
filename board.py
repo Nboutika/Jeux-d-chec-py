c48b8248f3cd521c75d4c0a6715979e1b01fa1e8
ARIES Lucas <lucas.aries@sfr.fr>
2020-10-23 17:54:21 +0200

    Trouvé qui gagne et jouer en boucle

--- board.py
+++ board.py
@@ -24,146 +24,160 @@ class Board():
 
 """
     Représentation des coordonnées du plateau: 
-    0 1 2 3 4 5 6 7 
+    1  2  3 4 5  6 7  8 
 ---------------------
-0|  ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-1|  ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎
-2|  -  - -  - - -  - -
+1|  ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+2|  ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎
 3|  -  - -  - - -  - -
 4|  -  - -  - - -  - -
 5|  -  - -  - - -  - -
-6|  ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-7|  ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+6|  -  - -  - - -  - -
+7|  ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+8|  ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 """
 
 b = Board()
 
 
-print("Pour jouez utiliser |ligne de la pièce| |colonne de la pièce| |ligne d'arrivé| |colonne d'arrivé|")
-commande = input("Jouez votre coup : ").split(" ")
-commandeTableau = []
+roiNoir=True
+roiBlanc=True
 
-for i in commande:
-    if i != "":
-        commandeTableau.append(int(i))
+while roiNoir and roiBlanc == True :
+    print("Pour jouez utiliser |ligne de la pièce| |colonne de la pièce| |ligne d'arrivé| |colonne d'arrivé|")
+    commande = input("Jouez votre coup : ").split(" ")
+    commandeTableau = []
 
-for i in range(len(commandeTableau)) :
-    if commandeTableau[i]<0 or commandeTableau[i]>7 :
-        print("Pas dans l'intervalle")
-    else :
-        print("Dans l'intervalle")
+    for i in commande:
+        if i != "":
+            commandeTableau.append(int(i))
 
-ligne=commandeTableau[0]
-colonne=commandeTableau[1]
-ligneArrive=commandeTableau[2]
-colonneArrive=commandeTableau[3]
+    for i in range(len(commandeTableau)) :
+        if commandeTableau[i]<0 or commandeTableau[i]>7 :
+            print("Pas dans l'intervalle")
+        else :
+            print("Dans l'intervalle")
 
-print(ligne,colonne,ligneArrive,colonneArrive)
+    ligne=commandeTableau[0]-1
+    colonne=commandeTableau[1]-1
+    ligneArrive=commandeTableau[2]-1
+    colonneArrive=commandeTableau[3]-1
 
-"""
+    print(ligne,colonne,ligneArrive,colonneArrive)
 
-print(ligne,colonne,ligneArrive,colonneArrive)
+    if ligne==ligneArrive and colonne==colonneArrive:
+        print("Il faut forcément faire un mouvement")
+    else:
+        if b.BoardCoord[ligne][colonne]== "♜":
+            print("tour blanche")
+            if tour(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
 
 
-if ligne==ligneArrive and colonne==colonneArrive:
-    print("Il faut forcément faire un mouvement")
+        if b.BoardCoord[ligne][colonne]== "♞":
+            print("cavalier blanc")
+            if cavalier(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♝":
+            print("fou blanc")
+            if fou(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")        
+
+        if b.BoardCoord[ligne][colonne]== "♛":
+            print("reine blanc")
+            if dame(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♚":
+            print("roi blanc")
+            if roi(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♟︎":
+            print("pion blanc ")
+            if pionNoir(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) : #pas encore de fonction pion blanc
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♖":
+            print("tour noir")
+            if tour(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♘":
+            print("cavalier noir")
+            if cavalier(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♗":
+            print("fou noir")
+            if fou(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♕":
+            print("reine noir")
+            if dame(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♔":
+            print("roi noir")
+            if roi(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+        if b.BoardCoord[ligne][colonne]== "♙":
+            print("pion noir ")
+            if pionNoir(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
+                print("mouvement possible")
+            else:
+                print("mouvement pas possible")
+
+
+
+        b.BoardCoord[ligneArrive][colonneArrive]=b.BoardCoord[ligne][colonne]
+        b.BoardCoord[ligne][colonne]="-"
+
+    for l in range(8) :
+        L=l+1
+        print(L," ",end="")
+        for c in range(8) :
+            print(b.BoardCoord[l][c]," ",end="")
+        print("")
+
+    print("   1  2  3  4  5  6  7  8")
+
+    roiNoir=False
+    roiBlanc=False
+
+    for x in range(8):
+        for y in range(8):
+            if b.BoardCoord[x][y] == "♔":
+                roiNoir=True
+            if b.BoardCoord[x][y] == "♚":
+                roiBlanc=True
+
+
+if roiNoir == False:
+    print("Le joueur blanc a gagné")
 else:
-    if b.BoardCoord[ligne][colonne]== "♜":
-        print("tour blanche")
-        if tour(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-
-    if b.BoardCoord[ligne][colonne]== "♞":
-        print("cavalier blanc")
-        if cavalier(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♝":
-        print("fou blanc")
-        if fou(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")        
-
-    if b.BoardCoord[ligne][colonne]== "♛":
-        print("reine blanc")
-        if dame(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♚":
-        print("roi blanc")
-        if roi(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♟︎":
-        print("pion blanc ")
-        if pionNoir(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) : #pas encore de fonction pion blanc
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♖":
-        print("tour noir")
-        if tour(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♘":
-        print("cavalier noir")
-        if cavalier(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♗":
-        print("fou noir")
-        if fou(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♕":
-        print("reine noir")
-        if dame(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♔":
-        print("roi noir")
-        if roi(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-    if b.BoardCoord[ligne][colonne]== "♙":
-        print("pion noir ")
-        if pionNoir(b.BoardCoord,ligne,colonne,ligneArrive,colonneArrive) :
-            print("mouvement possible")
-        else:
-            print("mouvement pas possible")
-
-
-
-    b.BoardCoord[ligneArrive][colonneArrive]=b.BoardCoord[ligne][colonne]
-    b.BoardCoord[ligne][colonne]="-"
-
-for l in range(8) :
-    print(l," ",end="")
-    for c in range(8) :
-        print(b.BoardCoord[l][c]," ",end="")
-    print("")
-
-print("   a  b  c  d  e  f  g  h")
-
-"""
+    print("Le joueur noir a gagné")
