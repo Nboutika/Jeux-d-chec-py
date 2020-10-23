df5b6daccd9d85c097fb21d8ba1d0c960541f187
nassim boutikar <boutikarnassim@gmail.com>
2020-10-24 00:10:15 +0200

    Refonte du cavalier anciennement mal codé

--- pieces.py
+++ pieces.py
@@ -2,11 +2,11 @@
 Initialisation des pièces
 
 La tour : Elle se déplace en ligne et colonne([+x][+0] OU [+0][+x])
-Le cavalier : Il se déplace en "L" donc il se déplace de 2 lignes et 1 colonne([1][0]) mais dans tous les sens 
+Le cavalier : Il se déplace en "L" donc il se déplace de 2 lignes et 1 colonne([1][0]) mais dans tous les sens
 Donc +-[2]+-[1] ET aussi +-[1]+-[2]
 Le fou : il se déplace toujours en diagonale [+x][+x]
 La dame : Elle a tous les mouvements des autres pièces sauf le cavalier
-Le roi : comme la dame mais de 1 en 1
+Le roi : comme lc na dame mais de 1 en 1
 Pion : les noirs se déplace en ligne [+x][0]
 Pion blanc : [-x][0]
 """
@@ -42,7 +42,7 @@ def tour(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
 
 
 def cavalier(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
-    return (ligne - 2 <= ligneArrive <= ligne + 2 and colonne - 1 <= colonneArrive <= colonne + 1) or (colonne - 2 <= colonneArrive <= colonne + 2 and ligne - 1 <= ligneArrive <= ligne + 1) and vide(boardCoord, ligneArrive, colonneArrive)
+    return ((ligneArrive == ligne - 2 or ligneArrive == ligne + 2) and (colonneArrive == colonne - 1 or colonneArrive == colonne + 1)) or ((colonneArrive == colonne - 2 or colonne + 2 and ligneArrive == ligne - 1 or ligneArrive == ligne + 1)) and vide(boardCoord, ligneArrive, colonneArrive)
 
 
 def fou(boardCoord,  ligne, colonne, ligneArrive, colonneArrive):
