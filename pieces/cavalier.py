11d1bec26da01f162d4689d7c52cbdf105c4cfab
nassim boutikar <boutikarnassim@gmail.com>
2020-10-31 02:56:48 +0100

    cavalier operationnel

--- pieces/cavalier.py
+++ pieces/cavalier.py
@@ -1,2 +1,26 @@
-def cavalier(boardCoord,  ligne, colonne, ligneArrive, colonneArrive, couleur):
-    pass
+from board import boardCoord, pieceBlanc, pieceNoir
+from vide import vide
+from boardlimit import boardlimit
+
+
+def cavalier(boardCoord,  ligne, colonne, couleur):
+    legalmoves = []
+    if couleur == "n":
+        color = pieceBlanc
+    else:
+        color = pieceNoir
+
+    poscavalier = [ligne, colonne]
+    deplacement = [[2, 1], [2, -1], [-2, 1],
+                   [-2, -1], [-1, -2], [1, -2], [1, 2], [1, -2]]
+    for possibilite in deplacement:
+        row = poscavalier[0] + possibilite[0]
+        column = poscavalier[1] + possibilite[1]
+        if boardlimit(row, column) and vide(boardCoord, row, column):
+            legalmoves.append(boardCoord[row][column])
+        elif boardlimit(row, column) and boardCoord[row][column] in color:
+            legalmoves.append(boardCoord[row][column])
+    return legalmoves
+
+
+print(cavalier(boardCoord, 0, 6, "n"))
