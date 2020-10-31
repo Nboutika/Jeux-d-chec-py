266807a36f824ac9f8a208a9b35dda4f3806ec8e
nassim boutikar <boutikarnassim@gmail.com>
2020-10-31 02:23:29 +0100

    fou opérationnel

--- pieces/fou.py
+++ pieces/fou.py
@@ -1,2 +1,26 @@
+from board import boardCoord, pieceBlanc, pieceNoir
+from vide import vide
+from boardlimit import boardlimit
+
+
 def fou(boardCoord,  ligne, colonne, ligneArrive, colonneArrive, couleur):
-    pass
+    legalmoves = []
+    if couleur == "n":
+        color = pieceBlanc
+    else:
+        color = pieceNoir
+    posfou = [ligne, colonne]
+    deplacement = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+    for possibilite in deplacement:
+        i = 1
+        while True:
+            row = posfou[0] + i * possibilite[0]
+            column = posfou[0] + i * possibilite[1]
+            if vide(boardCoord, row, column) and boardlimit(row, column):
+                legalmoves.append(boardCoord[row][column])
+                i += 1
+            else:
+                if boardCoord[row][column] in color and boardlimit(row, column):
+                    legalmoves.append(boardCoord[row][column])
+                break
+    return boardCoord[ligneArrive][colonneArrive] in legalmoves
