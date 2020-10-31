72fe9b26656c88c465d6d4c7d62cc47e1f3d919f
nassim boutikar <boutikarnassim@gmail.com>
2020-10-31 02:39:14 +0100

    Tour operationelle

--- pieces/tour.py
+++ pieces/tour.py
@@ -1,2 +1,26 @@
-def tour(boardCoord,  ligne, colonne, ligneArrive, colonneArrive, couleur):
-    pass
+from board import boardCoord, pieceBlanc, pieceNoir
+from vide import vide
+from boardlimit import boardlimit
+
+
+def tour(boardCoord, ligne, colonne, ligneArrive, colonneArrive, couleur):
+    legalmoves = []
+    if couleur == "n":
+        color = pieceBlanc
+    else:
+        color = pieceNoir
+    postour = [ligne, colonne]
+    deplacement = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    for possibilite in deplacement:
+        i = 1
+        while True:
+            row = postour[0] + i * possibilite[0]
+            column = postour[1] + i * possibilite[1]
+            if boardlimit(row, column) and vide(boardCoord, row, column):
+                legalmoves.append(boardCoord[row][column])
+                i += 1
+            else:
+                if boardlimit(row, column) and boardCoord[row][column] in color:
+                    legalmoves.append(boardCoord[row][column])
+                break
+    return boardCoord[ligneArrive][colonneArrive] in legalmoves
