47fad0945bfb86213756bbebbfb4c02d818ae13b
nassim boutikar <boutikarnassim@gmail.com>
2020-10-31 02:23:07 +0100

    roi opérationnel

--- pieces/roi.py
+++ pieces/roi.py
@@ -9,21 +9,16 @@ def roi(boardCoord, ligne, colonne, ligneArrive, colonneArrive, couleur):
         color = pieceBlanc
     else:
         color = pieceNoir
-    for lignes in boardCoord:
-        for colonnes in lignes:
-            if boardCoord.index(lignes) == ligne and lignes.index(colonnes) == colonne:
-                posroi = [ligne, colonne]
 
-                deplacement = [[1, 1], [1, -1],
-                               [0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [-1, -1]]
-                for possibilite in deplacement:
-                    row = posroi[0] + possibilite[0]
-                    column = posroi[1] + possibilite[1]
-                    if vide(boardCoord, row, column) and boardlimit(row, column):
-                        legalmoves.append(boardCoord[row][column])
-                    if boardCoord[row][column] in color and boardlimit(row, column):
-                        legalmoves.append(boardCoord[row][column])
+    posroi = [ligne, colonne]
+
+    deplacement = [[1, 1], [1, -1],
+                   [0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [-1, -1]]
+    for possibilite in deplacement:
+        row = posroi[0] + possibilite[0]
+        column = posroi[1] + possibilite[1]
+        if vide(boardCoord, row, column) and boardlimit(row, column):
+            legalmoves.append(boardCoord[row][column])
+        elif boardCoord[row][column] in color and boardlimit(row, column):
+            legalmoves.append(boardCoord[row][column])
     return boardCoord[ligneArrive][colonneArrive] in legalmoves
-
-
-print(roi(boardCoord, 0, 4, 1, 4, "n"))
