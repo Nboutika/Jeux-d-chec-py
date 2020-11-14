563b48985ef1c9955699b490c5cc1c9e36106f18
ARIES Lucas <lucas.aries@sfr.fr>
2020-11-14 16:11:07 +0100

    pull from Manissar

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from ressources.engine import Jeux
 from ressources.board import affichageBoard, resetBoard
 
@@ -9,13 +8,17 @@ Lancement du programme depuis ici
 """
 
 if __name__ == '__main__':
-    
-    lancer = input("Veuillez saisir 'y' pour lancer le jeu tout autre caractère pour fermer le jeu : ")
+    affichageBoard()
+    Jeux()
 
-    while lancer == "y" :
+    lancer = input(
+        "Veuillez saisir 'y' pour lancer le jeu tout autre caractère pour fermer le jeu : ").upper()
+
+    while lancer == "Y":
         resetBoard()
         affichageBoard()
         Jeux()
-        lancer = input("Veuillez saisir 'y' pour lancer le jeu tout autre caractère pour fermer le jeu : ")
+        lancer = input(
+            "Veuillez saisir 'y' pour lancer le jeu tout autre caractère pour fermer le jeu : ")
 
     print("Merci d'avoir joué")
