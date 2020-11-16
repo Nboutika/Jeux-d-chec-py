17e0271967e1b228378cccd77a7285beb22ff305
ARIES Lucas <lucas.aries@sfr.fr>
2020-11-16 21:26:15 +0100

    Version final

--- test/boardlimit.py
+++
@@ -1,3 +0,0 @@
-def boardlimit(ligneArrive, colonneArrive):
-    # limite de l'échiquier {0 - 7}
-    return ligneArrive > -1 and ligneArrive < 8 and colonneArrive > -1 and colonneArrive < 8
--- test/vide.py
+++
@@ -1,3 +0,0 @@
-def vide(boardCoord, ligneArrive, colonneArrive):
-    # si c'est un "-" la case est vide donc on vérifie si c'est un tiret
-    return boardCoord[ligneArrive][colonneArrive] == "-"
