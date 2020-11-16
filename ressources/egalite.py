d2fe4c617f45929ebad509382bfd9ced7d22d4f7
nassim boutikar <boutikarnassim@gmail.com>
2020-11-16 17:12:28 +0100

    pull from kallu

--- ressources/egalite.py
+++ ressources/egalite.py
@@ -55,16 +55,6 @@ def egalite(ligne, colonne, ligneArrive, colonneArrive, pieceJouer):
             historiqueFouB.extend([ligne, colonne, ligneArrive, colonneArrive])
             coupJouerTripleBlanc = clearTableauB("historiqueFouB")
 
-<<<<<<< HEAD
-            if len(historiqueFouB) > 24:
-                historiqueFouB[:4] = []
-
-            if len(historiqueFouB) == 24:
-                if (historiqueFouB[0] == historiqueFouB[6] == historiqueFouB[8]) and (historiqueFouB[1] == historiqueFouB[7] == historiqueFouB[9]) and (historiqueFouB[2] == historiqueFouB[4] == historiqueFouB[10]) and (historiqueFouB[3] == historiqueFouB[5] == historiqueFouB[11]):
-                    coupJouerTripleBlanc = True
-
-        if pieceJouer == br:  # correspond au roi blanc
-=======
             if len(historiqueFouB)> 24 :
                 historiqueFouB[:4]=[]
 
@@ -74,32 +64,10 @@ def egalite(ligne, colonne, ligneArrive, colonneArrive, pieceJouer):
 
 
         if pieceJouer ==br:  #correspond au roi blanc 
->>>>>>> 0f8ae819259a121193218e38187ea10e58cc0fb5
 
             historiqueRoiB.extend([ligne, colonne, ligneArrive, colonneArrive])
             coupJouerTripleBlanc = clearTableauB("historiqueRoiB")
 
-<<<<<<< HEAD
-            if len(historiqueRoiB) > 24:
-                historiqueRoiB[:4] = []
-
-            if len(historiqueRoiB) == 24:
-                if (historiqueRoiB[0] == historiqueRoiB[6] == historiqueRoiB[8]) and (historiqueRoiB[1] == historiqueRoiB[7] == historiqueRoiB[9]) and (historiqueRoiB[2] == historiqueRoiB[4] == historiqueRoiB[10]) and (historiqueRoiB[3] == historiqueRoiB[5] == historiqueRoiB[11]):
-                    coupJouerTripleBlanc = True
-
-        if pieceJouer == bd:  # correspond au la reine blanche
-
-            historiqueReineB.extend(
-                [ligne, colonne, ligneArrive, colonneArrive])
-            coupJouerTripleBlanc = clearTableauB("historiqueReineB")
-
-            if len(historiqueReineB) > 24:
-                historiqueReineB[:4] = []
-
-            if len(historiqueReineB) == 24:
-                if (historiqueReineB[0] == historiqueReineB[6] == historiqueReineB[8]) and (historiqueReineB[1] == historiqueReineB[7] == historiqueReineB[9]) and (historiqueReineB[2] == historiqueReineB[4] == historiqueReineB[10]) and (historiqueReineB[3] == historiqueReineB[5] == historiqueReineB[11]):
-                    coupJouerTripleBlanc = True
-=======
             if len(historiqueRoiB)> 24 :
                 historiqueRoiB[:4]=[]
 
@@ -119,43 +87,10 @@ def egalite(ligne, colonne, ligneArrive, colonneArrive, pieceJouer):
             if len(historiqueReineB) == 24 :
                 if (historiqueReineB[0]==historiqueReineB[6]==historiqueReineB[8]) and (historiqueReineB[1]==historiqueReineB[7]==historiqueReineB[9]) and (historiqueReineB[2]==historiqueReineB[4]==historiqueReineB[10]) and (historiqueReineB[3]==historiqueReineB[5]==historiqueReineB[11]):
                     coupJouerTripleBlanc=True
->>>>>>> 0f8ae819259a121193218e38187ea10e58cc0fb5
 
         return(coupJouerTripleBlanc)
 
     else:
-<<<<<<< HEAD
-        if pieceJouer == np:  # correspond au pion bnoir
-            coupJouerTripleNoir = clearTableauN("all")
-
-        if pieceJouer == nt:  # correspond a la tour noir
-
-            historiqueTourN.extend(
-                [ligne, colonne, ligneArrive, colonneArrive])
-            coupJouerTripleNoir = clearTableauN("historiqueTourN")
-
-            if len(historiqueTourN) > 24:
-                historiqueTourN[:4] = []
-
-            if len(historiqueTourN) == 24:
-                if (historiqueTourN[0] == historiqueTourN[6] == historiqueTourN[8]) and (historiqueTourN[1] == historiqueTourN[7] == historiqueTourN[9]) and (historiqueTourN[2] == historiqueTourN[4] == historiqueTourN[10]) and (historiqueTourN[3] == historiqueTourN[5] == historiqueTourN[11]):
-                    coupJouerTripleNoir = True
-
-        if pieceJouer == nc:  # correspond au cavalier noir
-
-            historiqueCavalierN.extend(
-                [ligne, colonne, ligneArrive, colonneArrive])
-            coupJouerTripleNoir = clearTableauN("historiqueCavalierN")
-
-            if len(historiqueCavalierN) > 24:
-                historiqueCavalierN[:4] = []
-
-            if len(historiqueCavalierN) == 24:
-                if (historiqueCavalierN[0] == historiqueCavalierN[6] == historiqueCavalierN[8] == historiqueCavalierN[14] == historiqueCavalierN[16] == historiqueCavalierN[22]) and (historiqueCavalierN[1] == historiqueCavalierN[7] == historiqueCavalierN[9] == historiqueCavalierN[15] == historiqueCavalierN[17] == historiqueCavalierN[23]) and (historiqueCavalierN[2] == historiqueCavalierN[4] == historiqueCavalierN[10] == historiqueCavalierN[12] == historiqueCavalierN[18] == historiqueCavalierN[20]) and (historiqueCavalierN[3] == historiqueCavalierN[5] == historiqueCavalierN[11] == historiqueCavalierN[13] == historiqueCavalierN[19] == historiqueCavalierN[21]):
-                    coupJouerTripleNoir = True
-
-        if pieceJouer == nf:  # correspond au fou noir
-=======
         if pieceJouer ==np: #correspond au pion bnoir
             coupJouerTripleNoir = clearTableauN("all")
 
@@ -186,21 +121,10 @@ def egalite(ligne, colonne, ligneArrive, colonneArrive, pieceJouer):
 
 
         if pieceJouer ==nf:  #correspond au fou noir
->>>>>>> 0f8ae819259a121193218e38187ea10e58cc0fb5
 
             historiqueFouN.extend([ligne, colonne, ligneArrive, colonneArrive])
             coupJouerTripleNoir = clearTableauN("historiqueFouN")
 
-<<<<<<< HEAD
-            if len(historiqueFouN) > 24:
-                historiqueFouN[:4] = []
-
-            if len(historiqueFouN) == 24:
-                if (historiqueFouN[0] == historiqueFouN[6] == historiqueFouN[8]) and (historiqueFouN[1] == historiqueFouN[7] == historiqueFouN[9]) and (historiqueFouN[2] == historiqueFouN[4] == historiqueFouN[10]) and (historiqueFouN[3] == historiqueFouN[5] == historiqueFouN[11]):
-                    coupJouerTripleNoir = True
-
-        if pieceJouer == nr:  # correspond au roi noir
-=======
             if len(historiqueFouN)> 24 :
                 historiqueFouN[:4]=[]
 
@@ -210,34 +134,10 @@ def egalite(ligne, colonne, ligneArrive, colonneArrive, pieceJouer):
 
 
         if pieceJouer ==nr:  #correspond au roi noir
->>>>>>> 0f8ae819259a121193218e38187ea10e58cc0fb5
 
             historiqueRoiN.extend([ligne, colonne, ligneArrive, colonneArrive])
             coupJouerTripleNoir = clearTableauN("historiqueRoiN")
 
-<<<<<<< HEAD
-            if len(historiqueRoiN) > 24:
-                historiqueRoiN[:4] = []
-
-            if len(historiqueRoiN) == 24:
-                if (historiqueRoiN[0] == historiqueRoiN[6] == historiqueRoiN[8]) and (historiqueRoiN[1] == historiqueRoiN[7] == historiqueRoiN[9]) and (historiqueRoiN[2] == historiqueRoiN[4] == historiqueRoiN[10]) and (historiqueRoiN[3] == historiqueRoiN[5] == historiqueRoiN[11]):
-                    coupJouerTripleNoir = True
-
-        if pieceJouer == nd:  # correspond a la reine noir
-
-            historiqueReineN.extend(
-                [ligne, colonne, ligneArrive, colonneArrive])
-            coupJouerTripleNoir = clearTableauN("historiqueReineN")
-
-            if len(historiqueReineN) > 24:
-                historiqueReineN[:4] = []
-
-            if len(historiqueReineN) == 24:
-                if (historiqueReineN[0] == historiqueReineN[6] == historiqueReineN[8]) and (historiqueReineN[1] == historiqueReineN[7] == historiqueReineN[9]) and (historiqueReineN[2] == historiqueReineN[4] == historiqueReineN[10]) and (historiqueReineN[3] == historiqueReineN[5] == historiqueReineN[11]):
-                    coupJouerTripleNoir = True
-
-        return(coupJouerTripleNoir)
-=======
             if len(historiqueRoiN)> 24 :
                 historiqueRoiN[:4]=[]
 
@@ -261,4 +161,3 @@ def egalite(ligne, colonne, ligneArrive, colonneArrive, pieceJouer):
         return(coupJouerTripleNoir)
 
 
->>>>>>> 0f8ae819259a121193218e38187ea10e58cc0fb5
